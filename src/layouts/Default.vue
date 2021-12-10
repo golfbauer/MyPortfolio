d69ff12fb58f504702b067687114131429bdfe3d
Henri Truetsch <htruet@stud.hs-heilbronn.de>
2021-12-10 23:04:21 +0000

    First commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-app>
+    <v-navigation-drawer width="240px" permanent>
+      <v-list-item>
+        <!-- <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+        </v-list-item-avatar> -->
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> Henri Truetsch </v-list-item-title>
+          <v-list-item-subtitle> Software Engineer </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <v-container>
+        <slot />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: "About", icon: "mdi-account-box-outline" },
+        { title: "Projects", icon: "mdi-apps" },
+        { title: "Education", icon: "mdi-school-outline" },
+        { title: "Contact", icon: "mdi-message-text-outline" },
+      ],
+    };
+  },
+};
+</script>
+
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+.v-application--wrap {
+  flex-direction: row;
+}
+
+.v-main {
+  flex: 1 0;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <Layout>
+
+    <section>
+      <v-container>
+        <v-row>
+          <v-col sm="6">
+            <g-image alt="Example image" src="~/profile.jpg" width="400" />
+          </v-col>
+          <v-col sm="6">
+            <h1>Hi, I'm Henri.</h1>
+            <p>I#m a Software Engineer...</p>
+          </v-col>
+        </v-row>
+      </v-container>
+    </section>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!",
+  },
+};
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
